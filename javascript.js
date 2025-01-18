2f6863d7c007a1cb8a22a24b1fb26c6b513b2b1b
Nishivaly <51959902+Nishivaly@users.noreply.github.com>
2025-01-18 16:54:42 -0300

    Fix nonbutton clicks changing display

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ let numOne;
 let operator;
 let numTwo;
 
-let currentDisplay = displayBox;
+let currentDisplay = displayBox.textContent;
 
 function updateDisplay() {
     currentDisplay = displayBox.textContent;
@@ -45,61 +45,63 @@ function divide(left, right) {
 }
 
 operands.addEventListener('click', (e) => {
-    if (displayBox.textContent == 0) {
-        displayBox.textContent = '';
-    }
-    switch (e.target.id) {
-        case '7':
-            displayBox.textContent += 7;
-            break;
-        case '8':
-            displayBox.textContent += 8;
-            break;
-        case '9':
-            displayBox.textContent += 9;
-            break;
-        case '/':
-            displayBox.textContent += '/';
-            break;
-        case '4':
-            displayBox.textContent += 4;
-            break;
-        case '5':
-            displayBox.textContent += 5;
-            break;
-        case '6':
-            displayBox.textContent += 6;
-            break;
-        case 'x':
-            displayBox.textContent += '*';
-            break;
-        case '1':
-            displayBox.textContent += 1;
-            break;
-        case '2':
-            displayBox.textContent += 2;
-            break;
-        case '3':
-            displayBox.textContent += 3;
-            break;
-        case '-':
-            displayBox.textContent += '-';
-            break;
-        case '0':
-            displayBox.textContent += 0;
-            break;
-        case 'dot':
-            displayBox.textContent += '.';
-            break;
-        case '=':
-            displayBox.textContent += '=';
-            break;
-        case '+':
-            displayBox.textContent += '+';
-            break;
+    if (e.target.tagName === 'BUTTON') {
+        if (displayBox.textContent == 0) {
+            displayBox.textContent = '';
+        }
+        switch (e.target.id) {
+            case '7':
+                displayBox.textContent += 7;
+                break;
+            case '8':
+                displayBox.textContent += 8;
+                break;
+            case '9':
+                displayBox.textContent += 9;
+                break;
+            case '/':
+                displayBox.textContent += ' / ';
+                break;
+            case '4':
+                displayBox.textContent += 4;
+                break;
+            case '5':
+                displayBox.textContent += 5;
+                break;
+            case '6':
+                displayBox.textContent += 6;
+                break;
+            case 'x':
+                displayBox.textContent += ' * ';
+                break;
+            case '1':
+                displayBox.textContent += 1;
+                break;
+            case '2':
+                displayBox.textContent += 2;
+                break;
+            case '3':
+                displayBox.textContent += 3;
+                break;
+            case '-':
+                displayBox.textContent += ' - ';
+                break;
+            case '0':
+                displayBox.textContent += 0;
+                break;
+            case 'dot':
+                displayBox.textContent += '.';
+                break;
+            case '=':
+                operate();
+                break;
+            case '+':
+                displayBox.textContent += ' + ';
+                break;
             case 'clear':
                 displayBox.textContent = 0;
                 break;
+        }
+        updateDisplay();
     }
-    updateDisplay();
 });
